beab261c2cd4fae764f45b3d28b4ee728df3d567
sudou-y <it-engineer@sudou-y.com>
2026-08-19 23:59:18 +0900

    202608192359

--- simple-web-site/simple-web-site.ps1
+++ simple-web-site/simple-web-site.ps1
@@ -7,11 +7,11 @@ $listener.Prefixes.Add($url)
 $listener.Start()
 
 Write-Output "Webサーバーを開始しました: $url"
-Write-Output "終了するには Ctrl + C を押してください。"
+Write-Output "終了するには ${url}stop にアクセスしてください。"
 
 $mimeTypes = @{
     '.html' = 'text/html; charset=utf-8';
-    '.css'  = 'test/css; charset=utf-8';
+    '.css'  = 'text/css; charset=utf-8';
     '.js'   = 'application/javascript; charset=utf-8';
     '.png'  = 'image/png';
     '.svg'  = 'image/svg+xml';
@@ -26,6 +26,14 @@ try {
         $request = $context.Request
         $response = $context.Response
 
+        if ($request.url.AbsolutePath -eq '/stop') {
+            $message = [System.Text.Encoding]::UTF8.GetBytes("Server stopped.")
+            $response.ContentType = 'text/plain; charset=utf-8'
+            $response.OutputStream.Write($message, 0, $message.Length)
+            $response.Close()
+            break;
+        }
+
         $relativePath = $request.Url.AbsolutePath.TrimStart('/').Replace('/', '\')
         $filePath = Join-Path $PSScriptRoot $relativePath
 
@@ -35,10 +43,9 @@ try {
 
             $response.StatusCode = 200
             $response.ContentLength64 = $bytes.Length
-
-            $contentType = ''
-            if ($mimeTypes.ContainsKey($ext)) {
-                $contentType = $mimeTypes[$ext]
+            $contentType = $mimeTypes[$ext]
+            if (-not $contentType) {
+                Write-Warning "MIMEタイプが未定義です: '$ext'"
             }
             $response.ContentType = $contentType
             $response.OutputStream.Write($bytes, 0, $bytes.Length)
@@ -51,4 +58,5 @@ try {
 }
 finally {
     $listener.Stop()
+    Write-Output "Webサーバーを停止しました: $url"
 }
